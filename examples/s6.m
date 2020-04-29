4ed2e7c835745fd756ac7dc8a38a767d650ad84d
Michael Smith <immir@immir.com>
2020-04-29 12:33:06 +1000

    add more examples

--- examples/s6.m
+++ examples/s6.m
@@ -2,12 +2,13 @@
 SetEchoInput(true);
 Attach("surf.m");
 
-P21111<D,a,b,c,k> := ProjectiveSpace(Rationals(), [2,1,1,1,1]);
-S5 := Scheme(P21111, [
+P211111<D,a,b,c,k,l> := ProjectiveSpace(Rationals(), [2,1,1,1,1,1]);
+S6 := Scheme(P211111, [
    16*D^2 - (a+b+c)*(-a+b+c)*(a-b+c)*(a+b-c),
-   4*k^2 - (2*b^2 + 2*c^2 - a^2)] );
+   4*k^2 - (2*b^2 + 2*c^2 - a^2),
+   4*l^2 - (2*a^2 + 2*c^2 - b^2)] );
 
-S := Veronese(S5);
+S := Veronese(S6);
 
 time C := Conics(S);
 #C;
